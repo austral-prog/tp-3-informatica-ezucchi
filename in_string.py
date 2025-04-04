6e0f83426b322df6ab16a1a8847df2fb297200fe
Eugenio Zucchi <ez.zucchieugenio@gmail.com>
2025-04-04 10:22:54 -0300

    First hand-in

--- in_string.py
+++ in_string.py
@@ -1,5 +1,11 @@
 def check_vowels():
     # Código a implementar utilizando input.
+    nombre = input(" #Input/n")
+    print(f"Contiene a: {"a" in nombre.lower()}")
+    print(f"Contiene e: {"e" in nombre.lower()}")
+    print(f"Contiene i: {"i" in nombre.lower()}")
+    print(f"Contiene o: {"o" in nombre.lower()}")
+    print(f"Contiene u: {"u" in nombre.lower()}")
 
 
 # Para verificar este ejercicio ejecutar el comando
--- slice_simple.py
+++ slice_simple.py
@@ -2,7 +2,9 @@ def slice_simple():
     texto = "Awesome"
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
-
+    print(f"{texto.lower()[:3]}")
+    print(f"{texto.lower()[2:5]}")
+    print(f"{texto.lower()}")
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
